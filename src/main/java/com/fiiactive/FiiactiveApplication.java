856297353b44da6d6931056ba2acb36b48d8f722
Sergiu221 <sergiu221@gmail.com>
2025-02-16 07:07:03 +0200

    Create SpringBoot project

---
+++ src/main/java/com/fiiactive/FiiactiveApplication.java
@@ -0,0 +1,13 @@
+package com.fiiactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiiactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiiactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiiactive/FiiactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiiactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiiactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
